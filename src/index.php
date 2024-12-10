fa8841c1f2c82f2a40cc4c2533f2ce78215792c3
ilyass anida <anidailyass@gmail.com>
2024-12-10 22:30:53 +0100

    #27 Insert countries data into the main page.

--- src/index.php
+++ src/index.php
@@ -9,9 +9,39 @@
     while ($data = mysqli_fetch_assoc($result)) {
         $countries[] = $data;
     }
+
+    function countryMapHtmlElement($country, $countryShortName, $x, $y) {
+        return "
+            <div class='absolute cursor-pointer group' style='left: $x%; top: $y%;'>
+                <div class='relative p-0.5 bg-primary rounded-sm z-20 group-hover:scale-[2.75] group-hover:rounded-b-none group-hover:z-30 duration-500 transition-all'>
+                    <div class='flag max-w-10'>
+                        <img class='w-full min-h-7' src='https://flagcdn.com/w320/$countryShortName.png' alt='$country'>
+                    </div>
+
+                    <div class='absolute opacity-0 -z-10 flex  justify-center items-center w-full left-0 top-0 group-hover:top-[98%] group-hover:opacity-100 transition-all duration-300'>
+                        <span class='w-full pb-0.5 text-center rounded-b-sm bg-primary px-0.5 text-[.35rem] text-white'>$country</span>
+                    </div>
+                </div>
+                <span class='absolute left-1/2 -translate-x-1/2 top-[98%] border-8 border-transparent border-t-primary'></span>
+            </div>
+        ";
+    }    
+    
+    function countryCardHtml($name, $imageUrl, $description) {
+        return "
+            <div class='h-56 cursor-pointer group relative rounded-lg overflow-hidden'>
+                <img class='min-h-full' src='$imageUrl' alt='$name'>
+                <div class='absolute top-0 right-0 group-hover:right-full transition-all duration-300 w-full h-full bg-primary bg-opacity-45'></div>
+                <div class='absolute top-0 left-0 group-hover:left-full transition-all duration-300 w-full h-full text-center p-2 py-4 text-white'>
+                    <span class='text-2xl font-bold'>$name</span>
+                    <p class='text-xs mt-7'>$description</p>
+                </div>
+            </div>
+        ";
+    }    
 ?>
 
-    <header class="flex justify-between container py-3">
+    <header class="flex justify-between container py-5">
         <a class="text-primary font-bold text-2xl" href="./">GEOAFRICA</a>
         <button class="bg-primary py-1 px-3 text-white rounded-lg flex gap-2 items-center"><i class="fa fa-plus"></i>Add Country</button>
     </header>
@@ -20,96 +50,18 @@
         <div class="max-w-2xl relative">
             <img class="w-full" src="../assets/images/africa (2).svg" alt="Africa Map">
 
-            <div class="absolute cursor-pointer left-[14%] top-[6%] group">
-                <div class="relative border-2 border-primary rounded-sm z-20 group-hover:scale-[2.5] duration-500 transition-all">
-                    <div class="flag max-w-8">
-                        <img class="w-full" src="https://flagcdn.com/w320/ma.png" alt="Morocco">
-                    </div>
-                    <div class="absolute overflow-hidden flex justify-center items-center w-full h-full bg-black left-0 top-0 opacity-0 bg-opacity-0 group-hover:bg-opacity-30 group-hover:opacity-100 transition-all duration-300">
-                        <span class="max-w-full px-0.5 text-[.35rem] text-white">Morocco</span>
-                    </div>
-                </div>
-                <span class="absolute left-1/2 -translate-x-1/2 top-[98%] border-8 border-transparent border-t-primary"></span>
-            </div>
+            <?php foreach ($countries as $country) {
+                echo countryMapHtmlElement($country["name"], $country["shortname"], $country["x"], $country["y"]);
+            }?>
         </div>
     </main>
 
     <div class="container pt-6 pb-12">
         <h1 class="text-center font-bold text-3xl mb-12">Explore Countries</h1>
         <div class="grid grid-cols-4 gap-2">
-
-            <div class="card cursor-pointer group relative rounded-lg overflow-hidden">
-                <img src="https://ilyassan.github.io/VisitMorocco/dev/assets/images/homepage/img_explore/safi.jpg" alt="morocco">
-                <div class="absolute top-0 right-0 group-hover:right-full transition-all duration-300 w-full h-full bg-primary bg-opacity-45"></div>
-                <div class="absolute top-0 left-0 group-hover:left-full transition-all duration-300 w-full h-full text-center p-2 py-4 text-white">
-                    <span class="text-2xl font-bold">Morocco</span>
-                    <p class="text-xs mt-7">Morocco, the "Ceramic City," is renowned for its pottery craftsmanship and the vibrant coastal life, blending rich history with scenic ocean views.</p>
-                </div>
-            </div>
-
-            <div class="card cursor-pointer group relative rounded-lg overflow-hidden">
-                <img src="https://ilyassan.github.io/VisitMorocco/dev/assets/images/homepage/img_explore/safi.jpg" alt="morocco">
-                <div class="absolute top-0 right-0 group-hover:right-full transition-all duration-300 w-full h-full bg-primary bg-opacity-45"></div>
-                <div class="absolute top-0 left-0 group-hover:left-full transition-all duration-300 w-full h-full text-center p-2 py-4 text-white">
-                    <span class="text-2xl font-bold">Morocco</span>
-                    <p class="text-xs mt-7">Morocco, the "Ceramic City," is renowned for its pottery craftsmanship and the vibrant coastal life, blending rich history with scenic ocean views.</p>
-                </div>
-            </div>
-
-            <div class="card cursor-pointer group relative rounded-lg overflow-hidden">
-                <img src="https://ilyassan.github.io/VisitMorocco/dev/assets/images/homepage/img_explore/safi.jpg" alt="morocco">
-                <div class="absolute top-0 right-0 group-hover:right-full transition-all duration-300 w-full h-full bg-primary bg-opacity-45"></div>
-                <div class="absolute top-0 left-0 group-hover:left-full transition-all duration-300 w-full h-full text-center p-2 py-4 text-white">
-                    <span class="text-2xl font-bold">Morocco</span>
-                    <p class="text-xs mt-7">Morocco, the "Ceramic City," is renowned for its pottery craftsmanship and the vibrant coastal life, blending rich history with scenic ocean views.</p>
-                </div>
-            </div>
-
-            <div class="card cursor-pointer group relative rounded-lg overflow-hidden">
-                <img src="https://ilyassan.github.io/VisitMorocco/dev/assets/images/homepage/img_explore/safi.jpg" alt="morocco">
-                <div class="absolute top-0 right-0 group-hover:right-full transition-all duration-300 w-full h-full bg-primary bg-opacity-45"></div>
-                <div class="absolute top-0 left-0 group-hover:left-full transition-all duration-300 w-full h-full text-center p-2 py-4 text-white">
-                    <span class="text-2xl font-bold">Morocco</span>
-                    <p class="text-xs mt-7">Morocco, the "Ceramic City," is renowned for its pottery craftsmanship and the vibrant coastal life, blending rich history with scenic ocean views.</p>
-                </div>
-            </div>
-
-            <div class="card cursor-pointer group relative rounded-lg overflow-hidden">
-                <img src="https://ilyassan.github.io/VisitMorocco/dev/assets/images/homepage/img_explore/safi.jpg" alt="morocco">
-                <div class="absolute top-0 right-0 group-hover:right-full transition-all duration-300 w-full h-full bg-primary bg-opacity-45"></div>
-                <div class="absolute top-0 left-0 group-hover:left-full transition-all duration-300 w-full h-full text-center p-2 py-4 text-white">
-                    <span class="text-2xl font-bold">Morocco</span>
-                    <p class="text-xs mt-7">Morocco, the "Ceramic City," is renowned for its pottery craftsmanship and the vibrant coastal life, blending rich history with scenic ocean views.</p>
-                </div>
-            </div>
-
-            <div class="card cursor-pointer group relative rounded-lg overflow-hidden">
-                <img src="https://ilyassan.github.io/VisitMorocco/dev/assets/images/homepage/img_explore/safi.jpg" alt="morocco">
-                <div class="absolute top-0 right-0 group-hover:right-full transition-all duration-300 w-full h-full bg-primary bg-opacity-45"></div>
-                <div class="absolute top-0 left-0 group-hover:left-full transition-all duration-300 w-full h-full text-center p-2 py-4 text-white">
-                    <span class="text-2xl font-bold">Morocco</span>
-                    <p class="text-xs mt-7">Morocco, the "Ceramic City," is renowned for its pottery craftsmanship and the vibrant coastal life, blending rich history with scenic ocean views.</p>
-                </div>
-            </div>
-
-            <div class="card cursor-pointer group relative rounded-lg overflow-hidden">
-                <img src="https://ilyassan.github.io/VisitMorocco/dev/assets/images/homepage/img_explore/safi.jpg" alt="morocco">
-                <div class="absolute top-0 right-0 group-hover:right-full transition-all duration-300 w-full h-full bg-primary bg-opacity-45"></div>
-                <div class="absolute top-0 left-0 group-hover:left-full transition-all duration-300 w-full h-full text-center p-2 py-4 text-white">
-                    <span class="text-2xl font-bold">Morocco</span>
-                    <p class="text-xs mt-7">Morocco, the "Ceramic City," is renowned for its pottery craftsmanship and the vibrant coastal life, blending rich history with scenic ocean views.</p>
-                </div>
-            </div>
-
-            <div class="card cursor-pointer group relative rounded-lg overflow-hidden">
-                <img src="https://ilyassan.github.io/VisitMorocco/dev/assets/images/homepage/img_explore/safi.jpg" alt="morocco">
-                <div class="absolute top-0 right-0 group-hover:right-full transition-all duration-300 w-full h-full bg-primary bg-opacity-45"></div>
-                <div class="absolute top-0 left-0 group-hover:left-full transition-all duration-300 w-full h-full text-center p-2 py-4 text-white">
-                    <span class="text-2xl font-bold">Morocco</span>
-                    <p class="text-xs mt-7">Morocco, the "Ceramic City," is renowned for its pottery craftsmanship and the vibrant coastal life, blending rich history with scenic ocean views.</p>
-                </div>
-            </div>
-
+            <?php foreach ($countries as $country) {
+                echo countryCardHtml($country['name'], $country['image_url'], $country['description']);
+            }?>
         </div>
 
         <button class="mx-auto bg-primary mt-8 py-1 px-3 text-white rounded-lg flex gap-2 items-center"><i class="fa fa-plus"></i>Add Country</button>
