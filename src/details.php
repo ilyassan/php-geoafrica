9a778a30a1afe5b115f0e33836fc1cf9a9d65602
ilyass anida <anidailyass@gmail.com>
2024-12-11 23:32:05 +0100

    #39 Implement the ability to add city to a country using API without refreshing the page.

--- src/details.php
+++ src/details.php
@@ -79,81 +79,110 @@
                 <div class="relative">
                     <div class="relative">
                         <input id="city" class="bg-[#eee] rounded-lg py-1 pl-2 pr-8 outline-none" type="text" placeholder="Add a city">
-                        <i class="absolute cursor-pointer text-primary text-xl right-1 top-1/2 -translate-y-1/2 fa fa-plus"></i>
+                        <i id="add-city" class="absolute cursor-pointer text-primary text-xl right-1 top-1/2 -translate-y-1/2 fa fa-plus"></i>
                     </div>
                     <div id="cities" class="flex hidden overflow-hidden absolute top-[110%] z-10 bg-[#eee] rounded-lg w-full flex-col">
-                    <?php
-                        if (!empty($unShowedCities)) {
-                            $last_city = end($unShowedCities);
-
-                            foreach ($unShowedCities as $city) {
-                                $cityId = $city['id_city'];
-                                $cityName = $city['name'];
-                                
-                                $style = $city == $last_city ? "": "border-b";
-                                echo "<span data-id='$cityId' class='cursor-pointer hover:bg-slate-200 px-2 py-1 $style border-b-black'>$cityName</span>";
-                            }
-                        } else {
-                            echo "<span class='px-2 py-1 text-gray-500'>No cities available</span>";
-                        }
-                    ?>
                     </div>
                 </div>
-                <div class="flex gap-10 max-w-full overflow-y-hidden overflow-x-scroll no-scrollbar">
-                        <?php
-                            foreach($showedCities as $city){
-                                $cityName = $city["name"];
-                                echo "
-                                    <div style='border-top-left-radius: .3rem; border-bottom-left-radius: .3rem;' class='flex items-center gap-3 relative text-white bg-primary px-2'>
-                                        <i class='cursor-pointer text-sm rotate-180 fa-solid fa-delete-left'></i>
-                                        $cityName
-                                        <span class='absolute w-0 h-0 -right-[16px] border-y-[16px] border-y-transparent border-l-[16px] border-l-primary'></span>
-                                    </div>"
-                                ;
-                            }
-                        ?>
+                <div id="cities-tags" class="flex gap-10 flex-1 max-w-full overflow-y-hidden overflow-x-scroll no-scrollbar">
                 </div>
             </div>
 
-            <div class="grid grid-cols-4 gap-2">
-                <?php
-                    foreach($showedCities as $city){
-                        $cityName = $city["name"];
-                        $isCapital = $city["is_capital"]
-                                    ?
-                                    "<div class='absolute left-1 top-1 text-xs text-white font-bold bg-primary px-2 py-1 rounded-xl'>Capital</div>"
-                                    :"";
-                        echo "
-                            <div class='card cursor-pointer relative rounded-lg overflow-hidden'>
-                                <img src='https://ilyassan.github.io/VisitMorocco/dev/assets/images/homepage/img_explore/safi.jpg' alt='morocco'>
-                                <div class='flex items-center justify-center absolute top-0 right-0 w-full h-full bg-primary bg-opacity-45'>
-                                    <span class='text-2xl font-bold text-white'>$cityName</span>
-                                </div>
-                                $isCapital
-                            </div>";
-                    }
-                ?>
+            <div id="cities-cards" class="grid grid-cols-4 gap-2">
             </div>
         </div>
     </main>
 
     <script type="text/javascript">
+
+        const unShowedCities = <?= json_encode($unShowedCities)?>;
+        const showedCities = <?= json_encode($showedCities)?>;
+
         const cityInput = document.getElementById("city");
         const citiesOptionsContainer = document.getElementById("cities");
+        const citiesTagsContainer = document.getElementById("cities-tags");
+        const citiesCardsContainer = document.getElementById("cities-cards");
 
-        const citiesOptions = Array.from(citiesOptionsContainer.children);
-        citiesOptions.forEach(option => {
-            option.onmousedown = function(){
-                let cityId = option.getAttribute("data-id");
-                let city = option.textContent;
+        showData();
 
-                cityInput.value = city;
-                cityInput.setAttribute("data-id", cityId);
+        function showData(){
+            citiesOptionsContainer.innerHTML = "";
+            citiesTagsContainer.innerHTML = "";
+            citiesCardsContainer.innerHTML = "";
+
+            if (unShowedCities != 0) {
+                let lastCity = unShowedCities[unShowedCities.length - 1];
+
+                for (let city of unShowedCities) {
+                    let style = city == lastCity ? "": "border-b";
+                    cities.innerHTML += `<span data-id='${city['id_city']}' class='cursor-pointer hover:bg-slate-200 px-2 py-1 ${style} border-b-black'>${city['name']}</span>`;
+                }
+            } else {
+                cities.innerHTML = "<span class='px-2 py-1 text-gray-500'>No cities available</span>";
             }
-        });
+            for(let city of showedCities){
+                citiesTagsContainer.innerHTML += `
+                    <div style='border-top-left-radius: .3rem; border-bottom-left-radius: .3rem;' class='flex items-center gap-3 relative text-white bg-primary px-2'>
+                        <i class='cursor-pointer text-sm rotate-180 fa-solid fa-delete-left'></i>
+                        ${city["name"]}
+                        <span class='absolute w-0 h-0 -right-[16px] border-y-[16px] border-y-transparent border-l-[16px] border-l-primary'></span>
+                    </div>`;   
+            }
+
+            for( let city of showedCities){
+                citiesCardsContainer.innerHTML += `
+                    <div class='card cursor-pointer relative rounded-lg overflow-hidden'>
+                        <img src='https://ilyassan.github.io/VisitMorocco/dev/assets/images/homepage/img_explore/safi.jpg' alt='morocco'>
+                        <div class='flex items-center justify-center absolute top-0 right-0 w-full h-full bg-primary bg-opacity-45'>
+                            <span class='text-2xl font-bold text-white'>${city["name"]}</span>
+                        </div>
+                        ${city["is_capital"] == 1
+                            ?
+                            "<div class='absolute left-1 top-1 text-xs text-white font-bold bg-primary px-2 py-1 rounded-xl'>Capital</div>"
+                            :
+                            ""
+                        }
+                    </div>`;
+                }
+
+            const citiesOptions = Array.from(citiesOptionsContainer.children);
+            citiesOptions.forEach(option => {
+                option.onmousedown = function(){
+                    let cityId = option.getAttribute("data-id");
+                    let city = option.textContent;
+
+                    cityInput.value = city;
+                    cityInput.setAttribute("data-id", cityId);
+                }
+            });
+        }
+
 
         cityInput.onblur = closeOptionsContainer;
         cityInput.onfocus = openOptionsContainer;
+
+        const addCityButton = document.getElementById("add-city");
+        addCityButton.onclick = async function(){
+            let cityId = cityInput.getAttribute("data-id");
+            if (!cityId) return;
+            
+            cityInput.removeAttribute("data-id");
+            cityInput.value = "";
+
+            let cityIndex = unShowedCities.findIndex(city => city["id_city"] == cityId);
+
+            let city = unShowedCities.splice(cityIndex, 1)[0];
+            showedCities.push(city);
+            showData();
+
+            await fetch("./api/addCity.php", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({ id: cityId })
+                    });
+        }
 
         function closeOptionsContainer() {
             citiesOptionsContainer.classList.add("hidden");
