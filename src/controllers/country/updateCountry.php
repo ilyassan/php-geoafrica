f4e32cfa888836f35e9126a4abefee5cd308ea3f
ilyass anida <anidailyass@gmail.com>
2024-12-13 15:31:36 +0100

    #47 Validate submited inputs in the backend.

--- src/controllers/country/updateCountry.php
+++ src/controllers/country/updateCountry.php
@@ -1,17 +1,25 @@
 <?php
+session_start();
 include("../../inc/database.php");
 
 $population = filter_input(INPUT_POST, 'population', FILTER_SANITIZE_NUMBER_INT);
 $countryId = filter_input(INPUT_POST, 'id_country', FILTER_SANITIZE_NUMBER_INT);
 $languageId = filter_input(INPUT_POST, 'id_language', FILTER_SANITIZE_NUMBER_INT);
 
-if ($population && $countryId && $languageId) {
+if ($countryId && $languageId && $population && $population >= 1) {
     $sql = "UPDATE countries 
             SET population = $population, id_language = $languageId 
             WHERE id_country = $countryId";
     
     mysqli_query($conn, $sql);
-    header("Location: " . $_SERVER['HTTP_REFERER']);
+    $_SESSION['status'] = 'success';
+    $_SESSION['message'] = 'Country updated successfully!';
+
+} else {
+    $_SESSION['status'] = 'error';
+    $_SESSION['message'] = 'Invalid inputs data.';
 }
+
+header("Location: " . $_SERVER['HTTP_REFERER']);
 
 ?>
--- src/inc/footer.php
+++ src/inc/footer.php
@@ -1,3 +1,22 @@
     <script src="../assets/js/loading.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <?php
+        if (isset($_SESSION['status']) && isset($_SESSION['message'])) {
+            $status = $_SESSION['status'];
+            $message = $_SESSION['message'];
+            
+            unset($_SESSION['status']);
+            unset($_SESSION['message']);
+            echo "<script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    Swal.fire({
+                        icon: '$status',
+                        title: '".ucfirst($status)."',
+                        text: '$message',
+                    });
+                });
+            </script>";
+        }
+    ?>
 </body>
 </html>
